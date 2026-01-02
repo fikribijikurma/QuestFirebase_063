7aa9db2dce39758e221134cbdc0e335bc45cb051
fikribijikurma <auliyafikrirahmadani@gmail.com>
2026-01-02 22:02:29 +0700

    memanggil DataSiswaApp

--- app/src/main/java/com/example/questfirebase_063/MainActivity.kt
+++ app/src/main/java/com/example/questfirebase_063/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.questfirebase_063.ui.DataSiswaApp
 import com.example.questfirebase_063.ui.theme.QuestFirebase_063Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestFirebase_063Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                        DataSiswaApp(
+//                        name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestFirebase_063Theme {
-        Greeting("Android")
-    }
-}
