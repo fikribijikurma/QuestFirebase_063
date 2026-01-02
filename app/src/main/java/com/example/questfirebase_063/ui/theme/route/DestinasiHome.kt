437d50cbfd228abb130d9e591d1cf2d27d9a7e7f
fikribijikurma <auliyafikrirahmadani@gmail.com>
2026-01-02 21:39:56 +0700

    Tambah objek DestinasiHome untuk navigasi
    - Tambahkan object `DestinasiHome` yang mengimplementasikan `DestinasiNavigasi`
    - Atur route ke "home" dan titleRes ke `R.string.app_name`

---
+++ app/src/main/java/com/example/questfirebase_063/ui/theme/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebase_063.ui.theme.route
+
+import com.example.questfirebase_063.R
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
