69875f9371b4c213295d196e846d4ed44dd98796
fikribijikurma <auliyafikrirahmadani@gmail.com>
2026-01-02 21:41:19 +0700

    Tambahkan object DestinasiEntry untuk navigasi
    - Tambahkan object `DestinasiEntry` yang mengimplementasikan `DestinasiNavigasi`
    - Tentukan route sebagai "item_entry"
    - Tentukan title resource menggunakan `R.string.entry_siswa`

---
+++ app/src/main/java/com/example/questfirebase_063/ui/theme/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebase_063.ui.theme.route
+
+import com.example.questfirebase_063.R
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
