2d7138802d91494085e76e1b3fa32b36d08d3615
fikribijikurma <auliyafikrirahmadani@gmail.com>
2026-01-08 11:12:30 +0700

    Implementasi `DetailViewModel` untuk fitur detail siswa

--- app/src/main/java/com/example/questfirebase_063/viewmodel/Detailviewmodel.kt
+++ app/src/main/java/com/example/questfirebase_063/viewmodel/Detailviewmodel.kt
@@ -1,9 +1,29 @@
 package com.example.questfirebase_063.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questfirebase_063.model.Siswa
+import com.example.questfirebase_063.repository.RepositorySiswa
+import com.example.questfirebase_063.ui.theme.route.DestinasiDetail
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
