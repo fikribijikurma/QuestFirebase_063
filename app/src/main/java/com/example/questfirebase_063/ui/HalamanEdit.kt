f8a86c7f4572bd04429f6f3bbcc312f404e6c76c
fikribijikurma <auliyafikrirahmadani@gmail.com>
2026-01-08 11:25:01 +0700

    Implementasikan HalamanEdit dan perbarui EditSiswaScreen

--- app/src/main/java/com/example/questfirebase_063/ui/HalamanEdit.kt
+++ app/src/main/java/com/example/questfirebase_063/ui/HalamanEdit.kt
@@ -1,72 +1,26 @@
 package com.example.questfirebase_063.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.questfirebase_063.R
-import com.example.questfirebase_063.ui.theme.route.DestinasiDetail
-import com.example.questfirebase_063.viewmodel.DetailViewModel
-import com.example.questfirebase_063.viewmodel.StatusUIDetail
-import kotlinx.coroutines.launch
+import com.example.questfirebase_063.viewmodel.EditViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen(
+fun EditSiswaScreen(
     navigateBack: () -> Unit,
-    onEditClick: (Long) -> Unit,
+    onNavigateUp: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-    val uiState = viewModel.statusUIDetail
-
     Scaffold(
         topBar = {
-            DetailTopAppBar(
-                title = stringResource(DestinasiDetail.titleRes),
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
                 canNavigateBack = true,
-                navigateUp = navigateBack
+                navigateUp = onNavigateUp
             )
         },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-                    val idSiswa = (uiState as? StatusUIDetail.Success)?.satusiswa?.id ?: 0
-                    onEditClick(idSiswa.toLong())
-                },
-                shape = MaterialTheme.shapes.medium,
-                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Edit,
-                    contentDescription = stringResource(id = R.string.edit_siswa)
-                )
-            }
-        },
-        modifier = modifier
-    ) { innerPadding ->
-        ItemDetailContainer(
-            statusUIDetail = uiState,
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize(),
-            onDelete = {
-                viewModel.viewModelScope.launch {
-                    viewModel.hapusSatuSiswa()
-                    navigateBack()
-                }
-            }
-        )
-    }
-}
